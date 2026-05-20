2a524569692cad3213f1d450958c38db9c464ed1
cdcp-unir <cristiandavid.c9298@comunidadunir.net>
2026-05-19 22:08:27 -0500

    Version inicial Eureka

---
+++ back-end-eureka/src/test/java/relatos_de_papel_eureka/BackEndEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package relatos_de_papel_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
