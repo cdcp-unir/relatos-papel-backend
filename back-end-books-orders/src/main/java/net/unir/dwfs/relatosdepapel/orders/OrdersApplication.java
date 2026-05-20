eb3b6745ae93f1a22de37e1f7e5fc4d6889b2d2b
Will <desarrollo@ideasgroup.com.ec>
2026-05-19 21:40:57 -0500

    Init ms project orders

---
+++ back-end-books-orders/src/main/java/net/unir/dwfs/relatosdepapel/orders/OrdersApplication.java
@@ -0,0 +1,13 @@
+package net.unir.dwfs.relatosdepapel.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersApplication.class, args);
+	}
+
+}
