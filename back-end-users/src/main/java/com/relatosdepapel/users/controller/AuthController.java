7921b07d2c84a4392f87671ea16e2ca805f996ee
cdcp-unir <cristiandavid.c9298@comunidadunir.net>
2026-06-06 15:16:01 -0500

    Ajustes para docker

--- back-end-users/src/main/java/com/relatosdepapel/users/controller/AuthController.java
+++ back-end-users/src/main/java/com/relatosdepapel/users/controller/AuthController.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
--- back-end-users/src/main/java/com/relatosdepapel/users/controller/UserController.java
+++ back-end-users/src/main/java/com/relatosdepapel/users/controller/UserController.java
@@ -4,15 +4,13 @@ import com.relatosdepapel.users.dto.CreateUserRequest;
 import com.relatosdepapel.users.entity.User;
 import com.relatosdepapel.users.service.UserService;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.relatosdepapel.users.dto.UserResponse;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api/v1/users")
 public class UserController {
 
     private final UserService userService;
@@ -21,12 +19,12 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public List<UserResponse> getUsers() {
         return userService.getAllUsers();
     }
 
-    @PostMapping ("/users")
+    @PostMapping
     public User createUser(@Valid @RequestBody CreateUserRequest request) {
         return userService.createUser(request);
     }
