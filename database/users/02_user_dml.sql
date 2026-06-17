cd858dd8d1c0c1f9f59b83acecbc0a204490c8d2
cdcp-unir <cristiandavid.c9298@comunidadunir.net>
2026-06-16 23:53:03 -0500

    Objetos sql para usuario

---
+++ database/users/02_user_dml.sql
@@ -0,0 +1,13 @@
+\connect
+users_db;
+
+INSERT INTO users (first_name,
+                   last_name,
+                   email,
+                   password,
+                   role)
+VALUES ('Comunidad',
+        'UNIR',
+        'administrador@unir.net',
+        '$2a$10$K.kgKpr5FcYotV1p/7RZ5OoXqXBxqFAUTDNbCg3ThyBzz6mCGZEDC',
+        'admin');
