db6424d8457565cbe9cf949f8eadef2b35705cec
Will <desarrollo@ideasgroup.com.ec>
2026-06-18 22:54:00 -0500

    fix ws nativo

--- back-end-comms-service/src/main/java/com/relatosdepapel/comms_service/config/WebSocketConfig.java
+++ back-end-comms-service/src/main/java/com/relatosdepapel/comms_service/config/WebSocketConfig.java
@@ -22,7 +22,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
         registry.addEndpoint("/ws/chat")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
     }
 }
